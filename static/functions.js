e1bb775f311a59d075bf5906e2005ef025c0ff0d
justforjan <schneiderjan.2013@gmail.com>
2021-03-01 16:43:54 +0000

    Final adjustments

--- static/functions.js
+++ static/functions.js
@@ -1,26 +1,3 @@
-function passBuyString(symbol){
-    window.localStorage.setItem("buy_symbol", symbol);
-}
-
-function passSellString(symbol){
-    window.localStorage.setItem("sell_symbol", symbol);
-}
-
-function buyString(){
-    document.getElementById("buy").value =
-        window.localStorage.getItem("buy_symbol");
-}
-
-function sellString(){
-    document.getElementById("sell").value =
-        window.localStorage.getItem("sell_symbol");
-}
-
-function clearForm(){
-    window.localStorage.clear();
-}
-
-
 $(function () {
   $('[data-toggle="popover"]').popover()
 })
